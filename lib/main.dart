e4f6c8307095e06d13acb36f8ef139cf3490fef9
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-03-23 10:28:52 -0500

    fixed title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class  MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: '!Geaux Breauk',
+      title: '!Geaux Breauk!',
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: 'ProximaNova',
