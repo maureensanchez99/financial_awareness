ad8d7e74079bbb15011e5574e5a3e6aa5de8c842
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-03-22 22:37:16 -0500

    added container color to pages

--- lib/pages/new_entry.dart
+++ lib/pages/new_entry.dart
@@ -1,84 +1,92 @@
 import 'package:flutter/material.dart';
 
-class NewEntry extends StatelessWidget {
-  NewEntry({super.key});
+class NewEntry extends StatefulWidget {
+  const NewEntry({super.key});
 
+  @override
+  State<NewEntry> createState() => _NewEntryState();
+}
+
+class _NewEntryState extends State<NewEntry> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    Text(
-                      'Enter new scholarship info:',
-                      style: TextStyle(
-                        fontFamily: 'ProximaNova',
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      ),
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(163, 154, 172, 1),
+      ),
+      body: Container(  
+        color: const Color.fromRGBO(163, 154, 172, 1),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,  
+                children: [
+                  const Text(
+                    'Enter new scholarship info:',
+                    style: TextStyle(
+                      fontFamily: 'ProximaNova',
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
                     ),
-                    const SizedBox(height: 16.0),
-                    TextFormField( // first text field
-                      decoration: const InputDecoration(
-                        hintText: 'Scholarship Organization',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Enter organization scholarship is from';
-                        }
-                        return null;
-                      },
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextFormField( // First text field
+                    decoration: const InputDecoration(
+                      hintText: 'Scholarship Organization',
+                      border: OutlineInputBorder(),
                     ),
-                    const SizedBox(height: 16.0),
-                    TextFormField( // second text field
-                      decoration: const InputDecoration(
-                        hintText: 'Scholarship Amount',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Enter the scholarship value';
-                        }
-                        return null;
-                      },
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Enter organization scholarship is from';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextFormField( // Second text field
+                    decoration: const InputDecoration(
+                      hintText: 'Scholarship Amount',
+                      border: OutlineInputBorder(),
                     ),
-                    const SizedBox(height: 16.0),
-                    TextFormField( // third text field
-                      decoration: const InputDecoration(
-                        hintText: 'Scholarship Organization Link',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Enter organization site link scholarship is from';
-                        }
-                        return null;
-                      },
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Enter the scholarship value';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextFormField( // Third text field
+                    decoration: const InputDecoration(
+                      hintText: 'Scholarship Organization Link',
+                      border: OutlineInputBorder(),
                     ),
-                    const SizedBox(height: 16.0),
-                    ElevatedButton( // submit button
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          // Process data
-                        }
-                      },
-                      child: const Text('Submit'),
-                    ),
-                  ],
-                ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Enter organization site link scholarship is from';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton( // Submit button
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Process data
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/pages/scholarships.dart
+++ lib/pages/scholarships.dart
@@ -9,27 +9,37 @@ class Scholarships extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Container( //page title
-              width: double.infinity,
-              height: 80,
-              alignment: Alignment.center,
-              child: const Text(
-                "My Scholarships",
-                style: TextStyle(
-                  fontFamily: 'ProximaNova',
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
+      body: Container(
+        color: const Color.fromRGBO(163, 154, 172, 1),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20.0),
+              Container( //page title
+                width: double.infinity,
+                height: 80,
+                alignment: Alignment.center,
+                child: const Text(
+                  "My Scholarships",
+                  style: TextStyle(
+                    fontFamily: 'ProximaNova',
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {}, 
-              child: const Text('Enter new scholarship'),
-            ),
-          ],
+              SizedBox(height: 16.0,),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NewEntry()),
+                    );
+                }, 
+                child: const Text('Enter new scholarship'),
+              ),
+            ],
+          ),
         ),
       ),
     );
